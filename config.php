f6af2baafee70bc59a21995671c75d70344c5415
Victor Legacy <77228110+victorlegacy@users.noreply.github.com>
2024-07-10 16:43:49 +0100

    Added Remote connection

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 
 <?php
-$conn = mysqli_connect('localhost','root','','prostore');
+$conn = mysqli_connect('193.203.166.21','u727832680_userlms','B@Fz9NNj2','u727832680_lms');
 if(!$conn){
 	echo "not connected";
 }
